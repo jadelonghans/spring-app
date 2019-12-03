34a7359934662845650eb47331e1730a67775ad7
JeshaniMac <jasondangol@gmail.com>
2019-12-03 12:34:17 +0900

    First commit

---
+++ src/main/java/com/practice/MyApp/MyAppApplication.java
@@ -0,0 +1,13 @@
+package com.practice.MyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppApplication.class, args);
+	}
+
+}
